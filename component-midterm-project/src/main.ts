8df81517adbee074d174212a2a7b434715eebc96
Hnin Ei Shwe Yee <eishweyee3110@gmail.com>
2025-08-15 04:34:42 +0700

    implement tailwind

--- component-midterm-project/src/main.ts
+++ component-midterm-project/src/main.ts
@@ -1,12 +1,15 @@
-import './assets/main.css'
+// import './assets/main.css'
+import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import "./assets/index.css";
 
 const app = createApp(App)
+app.use(router);
 
 app.use(createPinia())
 app.use(router)
